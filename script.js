48d2791f7689ad7fc9b8cdc8ad381a47bbc55676
Roberto Morales <roberto@morales.com.co>
2021-05-03 19:40:50 -0500

    Comentarios, variables, cadenas, constantes

---
+++ script.js
@@ -0,0 +1,43 @@
+//HOla Mundo
+//alert('Hola Mundo');
+console.log('Hola Mundo');
+
+// comentario de línea
+/*
+Comentario de
+varias líneas
+ */
+
+console.log('Línea uno');
+console.log('Línea dos');
+
+// Variables
+
+let a = 5;
+console.log(a);
+ 
+// cadenas
+
+let nombre = 'Roberto';
+console.log("Hola mundo",nombre);
+console.log('Hola Mundo'+nombre);
+console.log(`Hola Mundo ${nombre}`);
+
+//let b = 4, c = 5, d = 6;
+
+let b = 4;
+let c = 5;
+let d = 6;
+
+let nombreCompleto = 'Roberto Morales';
+let nombre_completo = 'Roberto Morales';
+
+let $ = 1;
+let _ = 2;
+
+// Constantes
+
+const PI = 3.1416;
+const COLOR_BLANCO = '#FFFFFFF';
+//PI = 5;
+//const codigo = getId();
